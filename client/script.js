614d2a0a515744de56ed981c8cba5d43feed1783
saijyoshna1276 <144762198+saijyoshna1276@users.noreply.github.com>
2026-05-30 15:12:10 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -7,8 +7,30 @@ function addPost() {
   postDiv.innerHTML = `
     <h3>${title}</h3>
     <p>${content}</p>
+
+    <input class="commentInput" placeholder="Write a comment">
+    <button onclick="addComment(this)">Comment</button>
+
+    <div class="comments"></div>
     <hr>
   `;
 
   document.getElementById("posts").appendChild(postDiv);
+
+  document.getElementById("title").value = "";
+  document.getElementById("content").value = "";
+}
+
+function addComment(button) {
+  const post = button.parentElement;
+
+  const commentInput = post.querySelector(".commentInput");
+  const commentsDiv = post.querySelector(".comments");
+
+  const comment = document.createElement("p");
+  comment.textContent = commentInput.value;
+
+  commentsDiv.appendChild(comment);
+
+  commentInput.value = "";
 }
